e348ac160978938ba954f7137e7966d673fdac19
Pickhealer <47362439+Pickhealer@users.noreply.github.com>
2019-05-29 11:55:41 +0900

    register function complete

--- js/register.js
+++ js/register.js
@@ -7,19 +7,31 @@ const gender = document.querySelector('.gender');
 const phonenum = document.querySelector('.phone-number');
 const major = document.querySelector('.major');
 const studentnum = document.querySelector('.student-number');
-const submit = document.querySelector('.signup');
+const submit = document.querySelector('.form-register');
+const check_term = document.querySelector('.check-term');
 
 const valid_id = document.querySelector('.valid-id');
 const invalid_id = document.querySelector('.invalid-id');
 const valid_pw = document.querySelector('.valid-pw');
 const invalid_pw = document.querySelector('.invalid-pw');
+const valid_pwch = document.querySelector('.valid-pwch');
+const invalid_pwch = document.querySelector('.invalid-pwch');
 const valid_lastname = document.querySelector('.valid-lastname');
 const invalid_lastname = document.querySelector('.invalid-lastname');
+const valid_firstname = document.querySelector('.valid-firstname');
+const invalid_firstname = document.querySelector('.invalid-firstname');
+const valid_gender = document.querySelector('.valid-gender');
+const invalid_gender = document.querySelector('.invalid-gender');
+const valid_phonenum = document.querySelector('.valid-phonenum');
+const invalid_phonenum = document.querySelector('.invalid-phonenum');
+const valid_studentnum = document.querySelector('.valid-studentnum');
+const invalid_studnetnum = document.querySelector('.invalid-studentnum');
+const invalid_term = document.querySelector('.invalid-term');
 
 /*innerHTML -> firstchild*/
-id.addEventListener('input', function(){
+id.addEventListener('input', function () {
     let standard_id = /[a-zA-Z0-9]*@[a-zA-Z0-9]*\.([a-zA-Z0-9]*\.[a-zA-Z0-9]*|[a-zA-Z0-9]*)/;
-    if(standard_id.test(id.value)){
+    if (standard_id.test(id.value)) {
         invalid_id.classList.add('invisible');
         valid_id.classList.remove('invisible');
     } else {
@@ -28,8 +40,8 @@ id.addEventListener('input', function(){
     }
 });
 
-pw.addEventListener('input', function(){
-    if(isNaN(pw.value)){
+pw.addEventListener('input', function () {
+    if (isNaN(pw.value)) {
         invalid_pw.classList.add('invisible');
         valid_pw.classList.remove('invisible');
     } else {
@@ -38,8 +50,18 @@ pw.addEventListener('input', function(){
     }
 });
 
-lastname.addEventListener('input', function(){
-    if(isNaN(lastname.value)){
+pwch.addEventListener('input', function () {
+    if (pw.value == pwch.value) {
+        invalid_pwch.classList.add('invisible');
+        valid_pwch.classList.remove('invisible');
+    } else {
+        invalid_pwch.classList.remove('invisible');
+        valid_pwch.classList.add('invisible');
+    }
+});
+
+lastname.addEventListener('input', function () {
+    if (isNaN(lastname.value)) {
         invalid_lastname.classList.add('invisible');
         valid_lastname.classList.remove('invisible');
     } else {
@@ -48,59 +70,143 @@ lastname.addEventListener('input', function(){
     }
 });
 
-studentnum.addEventListener('input',function(){
-    if(!(studentnum.value.length > 9 && studentnum.value.length < 11)){
-        major.innerHTML = '';
+firstname.addEventListener('input', function () {
+    if (isNaN(firstname.value)) {
+        invalid_firstname.classList.add('invisible');
+        valid_firstname.classList.remove('invisible');
     } else {
+        invalid_firstname.classList.remove("invisible");
+        valid_firstname.classList.add("invisible");
+    }
+});
+
+gender.addEventListener('change', function () {
+    invalid_gender.classList.add('invisible');
+    valid_gender.classList.remove('invisible');
+});
+
+phonenum.addEventListener('input', function () {
+    let standard_phone = /^01\d{9}/;
+    if (standard_phone.test(phonenum.value)) {
+        invalid_phonenum.classList.add('invisible');
+        valid_phonenum.classList.remove('invisible');
+    } else {
+        invalid_phonenum.classList.remove("invisible");
+        valid_phonenum.classList.add("invisible");
+    }
+});
+
+studentnum.addEventListener('input', function () {
+    if (!(studentnum.value.length > 9 && studentnum.value.length < 11)) {
+        valid_studentnum.classList.add('invisible');
+        invalid_studnetnum.classList.remove('invisible');
+        major.textContent = "";
+    } else {
+        valid_studentnum.classList.remove('invisible');
+        invalid_studnetnum.classList.add('invisible');
         let standard_st = /\d{4}(\d{3})\d{3}/;
         let majornum = standard_st.exec(studentnum.value)[1];
         switch (majornum) {
             case '120':
-                major.innerHTML = '기계공학부';
+                major.textContent = '기계공학부';
                 break;
             case '136':
-                major.innerHTML = '컴퓨터공학부';
+                major.textContent = '컴퓨터공학부';
                 break;
             case '140':
-                major.innerHTML = '메카트로닉스학부';
+                major.textContent = '메카트로닉스학부';
                 break;
             case '151':
-                major.innerHTML = '디자인건축공학부';
+                major.textContent = '디자인건축공학부';
                 break;
             case '161':
-                major.innerHTML = '전기전자통신공학부';
+                major.textContent = '전기전자통신공학부';
                 break;
             case '174':
-                major.innerHTML = '에너지신소재화학공학부';
+                major.textContent = '에너지신소재화학공학부';
                 break;
             case '180':
-                major.innerHTML = '산업경영학부';
+                major.textContent = '산업경영학부';
                 break;
             default:
-                major.innerHTML = '';
+                major.textContent = '';
         }
     }
 });
 
-submit.addEventListener('submit', function(){
-
-
-
-
-
-
-    if(invalid_id.classList.contains("invisible")){
-        alert("Enter Email Address");
-        id.focus();
-        return false;
-    } else if (invalid_pw.classList.contains("invisible")){
-        alert("Enter Password");
-        pw.focus();
-        return false;
-    } else if (invalid_pwch.classList.contains("invisible")){
-        alert("Doesn't match Password and Confirm Password");
-        pwch.focus();
-        return false;
+check_term.addEventListener('click', function(){
+    if (check_term.checked == true) {
+        invalid_term.classList.add('invisible');
+    } else {
+        invalid_term.classList.remove('invisible');
     }
-    return true;
 });
+
+submit.onsubmit = function (event) {
+    if (!(id.value)) {
+        valid_id.classList.add('invisible');
+        invalid_id.classList.remove('invisible');
+    }
+    if (!(pw.value)) {
+        valid_pw.classList.add('invisible');
+        invalid_pw.classList.remove('invisible');
+    }
+    if (!(pwch.value)) {
+        valid_pwch.classList.add('invisible');
+        invalid_pwch.classList.remove('invisible');
+    }
+    if (!(lastname.value)) {
+        valid_lastname.classList.add('invisible');
+        invalid_lastname.classList.remove('invisible');
+    }
+    if (!(firstname.value)) {
+        valid_firstname.classList.add('invisible');
+        invalid_firstname.classList.remove('invisible');
+    }
+    if (!(phonenum.value)) {
+        valid_phonenum.classList.add('invisible');
+        invalid_phonenum.classList.remove('invisible');
+    }
+    if (!(studentnum.value)) {
+        valid_studentnum.classList.add('invisible');
+        invalid_studnetnum.classList.remove('invisible');
+    }
+    if (gender.options[gender.selectedIndex].text == 'Choose') {
+        valid_gender.classList.add('invisible');
+        invalid_gender.classList.remove('invisible');
+    }
+    if (check_term.checked != true) {
+        invalid_term.classList.remove('invisible');
+    }
+    if (valid_id.classList.contains("invisible")) {
+        alert("Enter Email Address.");
+        return false;
+    } else if (valid_pw.classList.contains("invisible")) {
+        alert("Enter Password.");
+        return false;
+    } else if (valid_pwch.classList.contains("invisible")) {
+        alert("Doesn't match Password and Confirm Password.");
+        return false;
+    } else if (valid_lastname.classList.contains("invisible")) {
+        alert("Enter your Lastname.");
+        return false;
+    } else if (valid_firstname.classList.contains("invisible")) {
+        alert("Enter your Firstname.");
+        return false;
+    } else if (valid_gender.classList.contains("invisible")) {
+        alert("Select your gender.");
+        return false;
+    } else if (valid_phonenum.classList.contains("invisible")) {
+        alert("Enter your Phone number.");
+        return false;
+    } else if (valid_studentnum.classList.contains("invisible")) {
+        alert("Enter your Student-ID");
+        return false;
+    } else if (!(invalid_term.classList.contains("invisible"))) {
+        alert("Agree Term of Services and Conditions");
+        return false;
+    } else {
+        alert("Register successfully");
+        return true;
+    }
+};
